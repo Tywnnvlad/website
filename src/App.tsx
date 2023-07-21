6ecb6e2646c2fb5e991a928347b95497f362f1a3
Gregory Cal <gcal@Laptop>
2023-07-21 13:14:01 -0600

    added a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { useState } from 'react';
-import { MantineProvider } from '@mantine/core';
-import Mysection from './component/Mysection';
+import { useState } from "react";
+import { MantineProvider } from "@mantine/core";
+import Mysection from "./component/Mysection";
+import Head from "./component/Head";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-	return (
-		<>
-			<div className='container'>
-				<Mysection
-					image={'src/assets/bg1.jpg'}
-					text={'Lorem, ipsum dolor.'}
-				/>
-				<Mysection
-					image={'src/assets/bg2.jpg'}
-					text={'Lorem, ipsum dolor.'}
-				/>
-				<Mysection
-					image={'src/assets/bg3.jpg'}
-					text={'Lorem, ipsum dolor.'}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container">
+        <Head />
+        <Mysection
+          image={"src/assets/bg1.jpg"}
+          text={"Lorem, ipsum dolor."}
+        />
+        <Mysection
+          image={"src/assets/bg2.jpg"}
+          text={"Lorem, ipsum dolor."}
+        />
+        <Mysection
+          image={"src/assets/bg3.jpg"}
+          text={"Lorem, ipsum dolor."}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/component/Mysection.tsx
+++ src/component/Mysection.tsx
@@ -1,35 +1,36 @@
-import { ClassNames, Image, Button, Text } from '@mantine/core';
-import './Mysection.css';
-import { useEffect, useRef } from 'react';
+import { ClassNames, Image, Button, Text } from "@mantine/core";
+// import './Mysection.css';
+import { useEffect, useRef } from "react";
 
 const Mysection = ({ image, text }) => {
-	return (
-		// <div className='section'>
-		// 	<div className='copy'>
-		// 		<h2>Lorem ipsum dolor sit amet.</h2>
-		// 	</div>
-		// 	<Image
-		// 		src='src/assets/bg1.jpg'
-		// 		fit='fill'
-		// 	/>
-		// 	<Button>Down</Button>
-		// </div>
-		<div className='section'>
-			<div className='copy'>
-				<Text
-					size='3rem'
-					color='white'>
-					{text}
-				</Text>
-			</div>
-			<Image
-				className='img'
-				src={image}
-				fit='fill'
-			/>
-			<div className='overlay'></div>
-		</div>
-	);
+  return (
+    // <div className='section'>
+    // 	<div className='copy'>
+    // 		<h2>Lorem ipsum dolor sit amet.</h2>
+    // 	</div>
+    // 	<Image
+    // 		src='src/assets/bg1.jpg'
+    // 		fit='fill'
+    // 	/>
+    // 	<Button>Down</Button>
+    // </div>
+    <div className="section">
+      <div className="copy">
+        <Text
+          size="3rem"
+          color="white"
+        >
+          {text}
+        </Text>
+      </div>
+      <Image
+        className="img"
+        src={image}
+        fit="fill"
+      />
+      <div className="overlay"></div>
+    </div>
+  );
 };
 
 export default Mysection;
