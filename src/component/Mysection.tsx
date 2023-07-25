b45060f51df70afbbfed5818e46ec7c03820f51b
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-07-24 23:50:52 -0600

    added a map

--- src/component/Mysection.tsx
+++ src/component/Mysection.tsx
@@ -37,20 +37,20 @@ const Mysection = forwardRef((props: sectionProp, ref) => {
 			className='section'
 			ref={compRef}>
 			<div className='copy'>
-				<Container size='30rem'>
+				<Container size='xl'>
 					<Grid
 						grow
 						style={{
 							color: 'white',
 						}}>
-						<Grid.Col xl={12}>
+						<Grid.Col md={12}>
 							<Text
 								size='3rem'
 								color='white'>
 								{title}
 							</Text>
 						</Grid.Col>
-						<Grid.Col xl={8}>
+						<Grid.Col md={8}>
 							<Text>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
 								dolores ipsam quaerat inventore laborum. Ad perspiciatis optio
@@ -58,14 +58,24 @@ const Mysection = forwardRef((props: sectionProp, ref) => {
 								tempora nisi quo! Aut, asperiores?
 							</Text>
 						</Grid.Col>
-						<Grid.Col xl={4}>
+						<Grid.Col md={4}>
 							<Text>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit.
 								Sapiente rem totam ipsum.
 							</Text>
 						</Grid.Col>
-						<Grid.Col xl={4}>{child}</Grid.Col>
-						<Grid.Col xl={8}>{child}</Grid.Col>
+						<Grid.Col md={4}>{child}</Grid.Col>
+						<Grid.Col md={8}>{child}</Grid.Col>
+						<Grid.Col md={12}>
+							<iframe
+								src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63255.85673307466!2d98.73571668753556!3d7.737622723844234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304e20be764e3d0d%3A0xbf9db8781a549f71!2sPhi%20Phi%20Islands!5e0!3m2!1sen!2sca!4v1690263942518!5m2!1sen!2sca'
+								width='800'
+								height='600'
+								style={{ border: 0 }}
+								allowfullscreen=''
+								loading='lazy'
+								referrerpolicy='no-referrer-when-downgrade'></iframe>
+						</Grid.Col>
 					</Grid>
 				</Container>
 			</div>
