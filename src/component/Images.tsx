15ee08ce5d40e5614600f1e570055990d9da79c0
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-07-28 00:10:54 -0600

    added carousel effect, need to fix different screen sizes

--- src/component/Images.tsx
+++ src/component/Images.tsx
@@ -1,65 +1,86 @@
-import { BackgroundImage, Grid, Container, Text, Image } from "@mantine/core";
-import { useImperativeHandle, forwardRef, useRef } from "react";
-import { Carousel } from "@mantine/carousel";
+import { BackgroundImage, Grid, Container, Text, Image } from '@mantine/core';
+import { useImperativeHandle, forwardRef, useRef } from 'react';
+import { Carousel } from '@mantine/carousel';
+import image1 from '../assets/bg1.jpg';
+import image2 from '../assets/bg2.jpg';
+import image3 from '../assets/bg3.jpg';
+
+const images = [image1, image2, image3];
 
 type sectionProp = {
-  backgroundImage: string;
-  img: string;
-  title: string;
-  message: string;
+	backgroundImage: string;
+	img: string;
+	title: string;
+	message: string;
 };
 
-const Images = forwardRef((props: sectionProp, ref) => {
-  const { backgroundImage, img, title, message } = props;
-  const compRef = useRef();
-  useImperativeHandle(ref, () => ({
-    scrollIntoView: () => {
-      compRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
-    },
-  }));
-  return (
-    <div
-      className="section"
-      ref={compRef}
-    >
-      <div className="copy">
-        <Container size="lg">
-          <Grid
-            grow
-            style={{
-              color: "giantsjj.1",
-            }}
-          >
-            <Grid.Col sm={12}>
-              <Text size="2rem">{title}</Text>
-            </Grid.Col>
-            <Grid.Col sm={12}>
-              <Text size="2rem">{message}</Text>
-            </Grid.Col>
-          </Grid>
-        </Container>
-      </div>
+const slides = images.map((url) => (
+	<Carousel.Slide key={url}>
+		<Image src={url} />
+	</Carousel.Slide>
+));
 
-      {/* break */}
-      <Carousel
-        slideSize="70%"
-        height={200}
-        slideGap="xs"
-        controlsOffset="lg"
-        loop
-        withIndicators
-      >
-        <Carousel.Slide>{img}</Carousel.Slide>
-        <Carousel.Slide>2</Carousel.Slide>
-        <Carousel.Slide>3</Carousel.Slide>
-      </Carousel>
-      <BackgroundImage
-        className="img"
-        src={backgroundImage}
-      />
-      <div className="overlay" />
-    </div>
-  );
+const Images = forwardRef((props: sectionProp, ref) => {
+	const { backgroundImage, img, title, message } = props;
+	const compRef = useRef();
+	useImperativeHandle(ref, () => ({
+		scrollIntoView: () => {
+			compRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		},
+	}));
+	return (
+		<div
+			className='section'
+			ref={compRef}>
+			<div className='copy'>
+				<Container size='lg'>
+					<Grid
+						grow
+						style={{
+							color: 'giantsjj.1',
+						}}>
+						<Grid.Col sm={12}>
+							<Text
+								size='2rem'
+								color='giantsjj.1'>
+								{title}
+							</Text>
+						</Grid.Col>
+						<Grid.Col sm={12}>
+							<Text
+								size='1.5rem'
+								color='giantsjj.1'>
+								{message}
+							</Text>
+						</Grid.Col>
+						{/* Need to fix and resize for smaller */}
+						<Grid.Col
+							xl={12}
+							style={{ minHeight: '600px' }}>
+							<center>
+								<Carousel
+									maw={900}
+									miw={550}
+									mx='0'
+									withIndicators
+									loop>
+									{slides}
+								</Carousel>
+							</center>
+						</Grid.Col>
+					</Grid>
+				</Container>
+			</div>
+
+			{/* break */}
+
+			<BackgroundImage
+				className='img'
+				src={backgroundImage}
+			/>
+			<div className='overlay' />
+		</div>
+	);
 });
 
 export default Images;
--- src/component/Location.tsx
+++ src/component/Location.tsx
@@ -1,74 +1,71 @@
-import { BackgroundImage, Grid, Container, Text, Image } from "@mantine/core";
-import { useImperativeHandle, forwardRef, useRef } from "react";
+import { BackgroundImage, Grid, Container, Text, Image } from '@mantine/core';
+import { useImperativeHandle, forwardRef, useRef } from 'react';
 
 type sectionProp = {
-  backgroundImage: string;
-  map: string;
-  title: string;
-  message: string;
+	backgroundImage: string;
+	map: string;
+	title: string;
+	message: string;
 };
 
 const Location = forwardRef((props: sectionProp, ref) => {
-  const { backgroundImage, map, title, message } = props;
-  const compRef = useRef();
-  useImperativeHandle(ref, () => ({
-    scrollIntoView: () => {
-      compRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
-    },
-  }));
-  return (
-    <div
-      className="section"
-      ref={compRef}
-    >
-      <div className="copy">
-        <Container size="lg">
-          <Grid
-            grow
-            style={{
-              color: "giantsjj.1",
-              fontSize: "1rem",
-            }}
-          >
-            <Grid.Col sm={12}>
-              <Text
-                size="2rem"
-                color="giantsjj.1"
-              >
-                {title}
-              </Text>
-            </Grid.Col>
-            <Grid.Col sm={12}>
-              <Text
-                size="1.3rem"
-                color="giantsjj.1"
-              >
-                {message}
-              </Text>
-            </Grid.Col>
-            <Grid.Col sm={12}>
-              <center>
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63255.85673307466!2d98.73571668753556!3d7.737622723844234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304e20be764e3d0d%3A0xbf9db8781a549f71!2sPhi%20Phi%20Islands!5e0!3m2!1sen!2sca!4v1690263942518!5m2!1sen!2sca"
-                  width="350"
-                  height="300"
-                  style={{ border: 0 }}
-                  allowfullscreen="true"
-                  loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </center>
-            </Grid.Col>
-          </Grid>
-        </Container>
-      </div>
-      <BackgroundImage
-        className="img"
-        src={backgroundImage}
-      />
-      <div className="overlay" />
-    </div>
-  );
+	const { backgroundImage, map, title, message } = props;
+	const compRef = useRef();
+	useImperativeHandle(ref, () => ({
+		scrollIntoView: () => {
+			compRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		},
+	}));
+	return (
+		<div
+			className='section'
+			ref={compRef}>
+			<div className='copy'>
+				<Container size='lg'>
+					<Grid
+						grow
+						style={{
+							color: 'giantsjj.1',
+							fontSize: '1rem',
+						}}>
+						<Grid.Col sm={12}>
+							<Text
+								size='2rem'
+								color='giantsjj.1'>
+								{title}
+							</Text>
+						</Grid.Col>
+						<Grid.Col sm={12}>
+							<Text
+								size='1.3rem'
+								color='giantsjj.1'>
+								{message}
+							</Text>
+						</Grid.Col>
+
+						{/* Need to fix and resize for smaller */}
+						<Grid.Col sm={12}>
+							<center>
+								<iframe
+									src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63255.85673307466!2d98.73571668753556!3d7.737622723844234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304e20be764e3d0d%3A0xbf9db8781a549f71!2sPhi%20Phi%20Islands!5e0!3m2!1sen!2sca!4v1690263942518!5m2!1sen!2sca'
+									width='600'
+									height='550'
+									style={{ border: 0 }}
+									allowfullscreen='true'
+									loading='lazy'
+									referrerpolicy='no-referrer-when-downgrade'></iframe>
+							</center>
+						</Grid.Col>
+					</Grid>
+				</Container>
+			</div>
+			<BackgroundImage
+				className='img'
+				src={backgroundImage}
+			/>
+			<div className='overlay' />
+		</div>
+	);
 });
 
 export default Location;
